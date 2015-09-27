e52bd5534105c268daf66c67fea6e623082f5a10
Camden Wagner <camden.i.wagner@ivarcode.net>
2015-09-26 20:20:41 -0400

    Firebase integration

--- php/contact.php
+++ php/contact.php
@@ -102,13 +102,13 @@ File: contact.php
 
 		<!-- All Javascript at the bottom of the page for faster page loading -->
 		<!-- First try for the online version of jQuery-->
-		<script src="http://code.jquery.com/jquery.js"></script>
+		<!--<script src="http://code.jquery.com/jquery.js"></script>
 		<!-- If no online access, fallback to our hardcoded version of jQuery -->
-		<script>window.jQuery || document.write('<script src="includes/js/jquery-1.8.2.min.js"><\/script>')</script>
+		<!--<script>window.jQuery || document.write('<script src="includes/js/jquery-1.8.2.min.js"><\/script>')</script>
 		<!-- Bootstrap JS -->
-		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<!--<script src="bootstrap/js/bootstrap.min.js"></script>
 		<!-- Custom JS -->
-		<script src="includes/js/script.js"></script>
+		<!--<script src="includes/js/script.js"></script>
 
 	</body>
 </html>
