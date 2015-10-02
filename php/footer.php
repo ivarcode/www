c66973bfb046a6887770523aecc65fde15fc124e
Camden Wagner <camden.i.wagner@ivarcode.net>
2015-10-02 14:58:51 -0400

    LinkedIn URL change

--- php/footer.php
+++ php/footer.php
@@ -22,7 +22,7 @@
 						<h6>Connect With Me</h6>
 						<ul class="unstyled">
 							<li><a href="https://plus.google.com/114141582683736470404/posts">Google</a></li>
-							<li><a href="https://www.linkedin.com/pub/camden-wagner/b2/9a1/670">LinkedIn</a></li>
+							<li><a href="https://www.linkedin.com/in/camdeniwagner">LinkedIn</a></li>
 							<li><a href="https://github.com/awesomemancam">GitHub</a></li>
 							<li><a href="http://www.chess.com/members/view/awesomemancam">Chess.com</a></li>
 						</ul>
