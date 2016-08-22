e1f67d7f014c9b9b95427ed6f40a7b3b057aea0d
Camden Wagner <camden.i.wagner@ivarcode.net>
2016-08-21 23:50:24 -0400

    basic markup - need words filled in

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ File: index.php
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!-- Website Title & Description -->
+		<title>ivarcode.net</title>
+		<meta name="description" content="">
+
 		<!-- Required meta tags always come first -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -41,6 +45,99 @@ File: index.php
 			</div>
 		</section>
 
+		<section id="what-i-do">
+			<div class="section-content">
+				<div class="container">
+					<h2>What I do</h2>
+					<p class="lead">I am an experienced computer programmer in many languages including but not limited to: Java, C, C++, HTML, Javascript, PHP, CSS, Python and XML.  I am currently working on some personal projects, as well as working at Connecticut College as a network technician and as technical support at the college's IT Service Desk.  I play chess in my free time online, tutor kids and adults, and play at a competitive level in <a href="https://new.uschess.org/home/">USCF tournaments</a>.</p>
+
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="card-group">
+								<div class="card">
+									<div class="card-block">
+										<h4 class="card-title">Work Experience</h4>
+										<h6 class="card-subtitle">subtitle</h6>
+									</div>
+									<img height="250" src="img/Servers_BackCables1.jpg" alt="server image">
+									<div class="card-block">
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+										<button type="button" class="btn btn-success-outline" data-toggle="modal" data-target="#myModal">learn more</button>
+									</div>
+								</div>
+
+								<div class="card">
+									<div class="card-block">
+										<h4 class="card-title">Projects</h4>
+										<h6 class="card-subtitle">Support card subtitle</h6>
+									</div>
+									<img height="250" width="369" src="img/html-code.jpg" alt="html code image">
+									<div class="card-block">
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi debitis fugit molestias dignissimos, id hic officiis quam possimus tenetur minima voluptatibus accusantium, numquam voluptates velit, dicta maiores dolor quo expedita.</p>
+										<button type="button" class="btn btn-success-outline" data-toggle="modal" data-target="#myModal">learn more</button>
+									</div>
+								</div>
+
+								<div class="card">
+									<div class="card-block">
+										<h4 class="card-title">Chess</h4>
+										<h6 class="card-subtitle">Support card subtitle</h6>
+									</div>
+									<img height="250" src="img/Knight-chess.jpg" alt="black_knight image">
+									<div class="card-block">
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi debitis fugit molestias dignissimos, id hic officiis quam possimus tenetur minima voluptatibus accusantium.</p>
+										<button type="button" class="btn btn-success-outline" data-toggle="modal" data-target="#myModal">learn more</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="about">
+			<div class="section-content">
+				<div class="container">
+					<div class="col-md-6">
+						<div class="about-text">
+							<img id="cwagner_hs" class="float_right" width="200" height="300" src="img/cwagner_hs.jpg" alt="cwagner_hs image file"></img>
+							<h3>About me</h3>
+							<p class="lead">I am Camden Wagner, currently a college student studying computer science at Connecticut College ('19).\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Minima culpa nostrum voluptates praesentium quia, quae, dolor aperiam possimus architecto, tempore unde! Quasi fugit voluptate, maiores adipisci commodi nemo rem cumque.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eum mollitia sit dolorem autem qui possimus ex voluptate, voluptatibus iste unde numquam illum, molestiae reprehenderit, eligendi. Illum quod esse voluptatibus.</p>
+
+							<h5>Follow me on the web</h5>
+							<a href="" class="btn btn-sm btn-secondary-outline">twitter</a>
+							<a href="" class="btn btn-sm btn-secondary-outline">facebook</a>
+							<a href="" class="btn btn-sm btn-secondary-outline">youtube</a>
+							<p><b>email me at</b>camden.i.wagner@ivarcode.net</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer id="footer-main">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<h6>Copyright &copy; 2016 C.I.W.</h6>
+						<p>coded by Camden I. Wagner</p>
+					</div>
+					<div class="col-sm-3">
+						
+					</div>
+					<div class="col-sm-5"></div>
+					<div class="col-sm-1">
+						<ul class="list-unstyled">
+							<li><a href="">home</a></li>
+							<li><a href="">what i do</a></li>
+							<li><a href="">about me</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
 
 		<!-- jQuery first, then Tether, then Bootstrap JS. -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
