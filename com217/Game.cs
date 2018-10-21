5459d0984abdddf435169588b4046c79b65106e5
Camden Wagner <camden.i.wagner@ivarcode.net>
2018-10-21 15:30:13 -0400

    work on adding com217 subsite (also some work on the game scripts for this class are included temporarily because I am lazy and don't want to store them in a new repo)

---
+++ com217/Game.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    // KEY
+    /*
+        0 - empty
+        1 - block
+
+     */
+
+    // Game objects
+    int[,] room = new int[,]
+    {
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    };
+
+	public Mob waterMob;
+	public Mob airMob;
+	public Mob earthMob;
+	public Mob iceMob;
+	public Mob fireMob;
+
+	public PlayerController player;
+	public Wall waterObstacle;
+
+	// Use this for initialization
+	void Start () {
+		Mob newDummy;
+		Wall newObstacle;
+        Vector3 position;
+
+		// anything spawned with an x coordinate between 8 and -8 will be in the field
+		// anything spawned with a y coordinate between 4 and -4 will be in the field. 
+
+        for (int i = 0; i < 17; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (room[j,i] == 1) {
+                    position = new Vector3(i-8,-j+4,0);
+                    newObstacle = Instantiate(waterObstacle, position, Quaternion.identity);
+                }
+            }
+        }
+
+        Debug.Log("ayyoo");
+
+		//creates 3 mobs
+		// Vector3 position = new Vector3 (0,-4,0);
+		// newDummy = Instantiate(waterMob, position, Quaternion.identity);
+
+		// position = new Vector3 (3,0,0);
+		// newDummy = Instantiate(waterMob, position,Quaternion.identity);
+
+		// position = new Vector3 (7,3,0);
+		// newDummy = Instantiate(waterMob, position, Quaternion.identity);
+
+
+		// //creates 3 blocks
+		// position = new Vector3 (-1, 2, 0);
+		// newObstacle = Instantiate(waterObstacle, position, Quaternion.identity);
+
+		// position = new Vector3 (-1, -2, 0);
+		// newObstacle = Instantiate(waterObstacle, position, Quaternion.identity);
+
+		// position = new Vector3 (4, -1, 0);
+		// newObstacle = Instantiate(waterObstacle, position, Quaternion.identity);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ com217/Room.cs
@@ -0,0 +1,36 @@
+public class Room {
+
+    public Room n, s, e, w;
+    public String type;
+    public int x, y;
+    public int[,] data = new int[,]
+    {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    };
+
+    public Room(int cx, int cy, String t) {
+        x = cx;
+        y = cy;
+        type = t;
+        n = null;
+        s = null;
+        e = null;
+        w = null;
+        generate();
+    }
+
+    public void generate() {
+        // will generate some 'stuff' in room based on the
+        // room type and neighbor data
+    }
+
+}
