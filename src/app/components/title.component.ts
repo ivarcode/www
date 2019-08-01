83f3e0960d36f94118ea92862ccc7ee5c8ebc1a6
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-08-01 19:49:56 -0400

    margin editing, committing before changing the auto-save formatting features

--- src/app/components/title.component.ts
+++ src/app/components/title.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-title',
   template: `
-    <h2>title component</h2>
+    <br />
+    <h2 class="my_font">ivarcode.net</h2>
   `
 })
 
