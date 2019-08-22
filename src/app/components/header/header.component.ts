3be7a13d2ae777806d0eaa6a62709ce6b4c38bf1
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-08-22 17:43:00 -0400

    typo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     template: `
         <div class="topnav" id="myTopnav">
             <a routerLink="/" class="active my_font" id="title">ivarcode.net</a>
-            <a routerLink="/what-i-do" class="topnavel my_font">what i do </a>
+            <a routerLink="/what-i-do" class="topnavel my_font">what i do</a>
             <a routerLink="/about-me" class="topnavel my_font">about me</a>
             <a href="http://schackmatt.net/" class="topnavel my_font">chess</a>
         </div>
